2f9393249aec5f91c3f77d3b51640dd75eb0c08d
sjoerdvds <s.j.vanderspoel@utwente.nl>
2015-04-10 14:48:40 +0200

    Package level documentation

---
+++ R/crtests.R
@@ -0,0 +1,7 @@
+#' crtests: A package for creating and executing classification and regression tests
+#' 
+#' The crtests package provides functions to prepare data, train models, test models, and evaluate outcomes. Its goal is to provide a generic and extendable API to machine learning and statistics functions
+#' 
+#' @docType package
+#' @name crtests
+NULL
